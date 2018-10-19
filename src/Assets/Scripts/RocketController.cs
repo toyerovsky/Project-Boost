225c251e028d9aead63a2c633f916a38921cf252
Przemysław P <toyerek@gmail.com>
2018-10-19 21:29:28 +0200

    Add rocket with basic thrust script

---
+++ src/Assets/Scripts/RocketController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RocketController : MonoBehaviour
+    {
+        private Rigidbody rigidbody;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            ProcessInput();
+        }
+
+        private void ProcessInput()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rigidbody.AddRelativeForce(Vector3.up);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+
+            }
+        }
+    }
+}
